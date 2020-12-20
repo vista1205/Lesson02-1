6ff31077a590904f91128375817522f271f41d2e
vista1205 <vista35@vtomske.ru>
2020-12-20 20:05:58 +0700

    Done task 1 and 2

---
+++ Lesson02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void dec_to_bin(int n)
+        {
+            if (n >= 2)
+                dec_to_bin(n / 2);
+            Console.Write(n % 2);
+        }
+        static int pow(int a, int b)
+        {
+            if (b == 1)
+                return (a);
+            return (a * pow(a, --b));
+        }
+        static void powst(int a, int b)
+        {
+            int c = a;
+            for (int i = 1; i < b; i++)
+                c = c * a;
+            Console.WriteLine(c);
+        }
+        static int pow2(int a, int b)
+        {
+            if (b == 0)
+                return 1;
+            int tmp = pow2(a, b / 2);
+            if (b % 2 == 0)
+                return tmp * tmp;
+            else
+                return tmp * tmp * a;
+        }
+        static void Main(string[] args)
+        {
+            dec_to_bin(10);
+            Console.WriteLine("\n" + pow(2, 10));
+            powst(2, 10);
+            Console.WriteLine(pow2(2, 10));
+            Console.ReadKey();
+        }
+    }
+}
